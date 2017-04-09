6782519c883252ef55b13d43f336b783e26db0ce
sebastianjorc <sebastianjorc@gmail.com>
2017-04-09 14:15:46 -0300

    Panel transparente

--- src/View/Interfaz.java
+++ src/View/Interfaz.java
@@ -1,11 +1,13 @@
 package View;
 
+import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
 public class Interfaz extends JFrame implements Colores{
 	private static final long serialVersionUID = 1L;
-	protected PanelFondo PanelContenedor;
+	protected PanelBase PanelContenedor;
+	PanelBase pfa, pfc;
 	
 	public Interfaz(){		
 		setResizable(false);
@@ -13,31 +15,36 @@ public class Interfaz extends JFrame implements Colores{
 		setTitle("Odontolog\u00EDa ULS");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(270, 76, 940, 600);
-		PanelContenedor = new PanelFondo();
+		PanelContenedor = new PanelBase();
 		PanelContenedor.setBackground(blanco);
 		setContentPane(PanelContenedor);
 		
 		LabelBase lu = new LabelBase ("Interfaz usuario: ");
 		PanelContenedor.add(lu);
-		PanelContenedor.setBackground();			
 	}
 	
 	public Interfaz(String title, int id, String name){		
+		
 		setResizable(false);
 		setBackground(blanco);
 		setTitle("Odontolog\u00EDa ULS");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(270, 76, 940, 600);
-		PanelContenedor = new PanelFondo();
+		PanelContenedor = new PanelBase("logo_xl.png");
+		PanelContenedor.setLayout(new BorderLayout());
 		PanelContenedor.setBackground(blanco);
 		setContentPane(PanelContenedor);
-		
-		PanelContenedor.add(new LabelBase ("\tInterfaz usuario: "+title));
-		PanelContenedor.add(new LabelBase ("\tId Usuario: "+id));
-		PanelContenedor.add(new LabelBase ("\tnombre Usuario: "+name));
-		
-		PanelContenedor.setBackground();
+
+		pfa = new PanelBase();
+		pfa.add(new LabelBase ("\tInterfaz usuario: "+title));
+		pfa.add(new LabelBase ("\tId Usuario: "+id));
+		pfa.add(new LabelBase ("\tnombre Usuario: "+name));		
+		PanelContenedor.add(pfa,BorderLayout.NORTH);
+
+		pfc = new PanelBase();
+		pfc.setSize(300, 300);
+		PanelContenedor.add(pfc);
+
 	}
 	
-
 }
---
+++ src/View/PanelBase.java
@@ -0,0 +1,47 @@
+package View;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class PanelBase extends JPanel implements Colores {	
+	
+	private static final long serialVersionUID = 1L;
+	private Image background;
+	
+	PanelBase(){
+		super();
+		//this.setBackground(blanco);
+		this.setBorder(BorderFactory.createLineBorder(navyblue));
+		this.setForeground(navyblue);
+		this.setOpaque(false);
+		this.background = new ImageIcon(getClass().getResource("transparent.png")).getImage();
+		repaint();
+	}
+	
+	public PanelBase(String background) {
+		this.setBackground(blanco);
+		this.setForeground(navyblue);
+		this.setOpaque(false);
+		this.background = new ImageIcon(getClass().getResource(background)).getImage();
+		repaint();
+	}
+
+	public void paintComponent(Graphics g) {
+
+		int width = this.getSize().width;
+		int height = this.getSize().height;
+		g.drawImage(this.background, 0, 0, width, height, null);			
+		super.paintComponent(g);
+		
+	}
+	
+	public void setBackground(String background) {
+		this.setOpaque(false);
+		this.background = new ImageIcon(getClass().getResource(background)).getImage();
+		repaint();
+	}
+}
